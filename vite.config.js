99ac142641657f0923fa3449c3096bc6541f84e0
liewchooichin <liewchooichin@gmail.com>
2024-10-08 14:38:57 +0800

    Try changing the proxy server for the Datamall problem. But not working also.

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// Attempt to solve the datamall problem. But, not working.
+// server: {
+//   proxy: {
+//     '/datamall': {
+//       target: 'https://datamall2.mytransport.sg',
+//       changeOrigin: true,
+//       rewrite: (path) => path.replace(/^\/datamall/, ''),
+//     },
+//     cors: false,
+//   }
+// }
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
